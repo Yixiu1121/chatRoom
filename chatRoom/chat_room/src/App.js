91eb4c5708843717c779018f3dcaf5ca40e7e173
Anna Chuang <anna.ih.chuang@viewsonic.com>
2024-04-24 00:25:43 +0800

    feat: add the login and chat page in app.js

--- chatRoom/chat_room/src/App.js
+++ chatRoom/chat_room/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import ChatRoom from "./containers/ChatRoom";
+import SignIn from "./containers/SignIn";
+import { useChat } from "./containers/hooks/useChat.js";
+import { useEffect } from "react";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 500px;
+  margin: auto;
+`;
 
 function App() {
+  const { status, signedIn, displayStatus } = useChat();
+
+  useEffect(() => {
+    displayStatus(status);
+  }, [status, displayStatus]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>{signedIn ? <ChatRoom></ChatRoom> : <SignIn></SignIn>}</Wrapper>
   );
 }
 
